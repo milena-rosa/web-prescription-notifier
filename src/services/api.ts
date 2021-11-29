f363b19c17b37fba1091dbcf66398191a25bea67
Milena Boselli Rosa <milena.rosa@gmail.com>
2021-11-29 19:51:59 -0300

    changed api endpoint.

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,7 +8,7 @@ export function setupAPIClient(ctx = undefined) {
   let cookies = parseCookies(ctx)
 
   const api = axios.create({
-    baseURL: 'http://localhost:3333',
+    baseURL: 'http://rest-api-prescription-notifier.herokuapp.com',
     headers: {
       Authorization: `Bearer ${cookies['prescription_notifier.token']}`
     }
